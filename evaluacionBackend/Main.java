e77a2d6c10aba4e3e0db437dd14fd421005f193d
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-11-14 03:41:02 -0600

    25.3 Agrega clase utileria con sus métodos básicos, así como listado de objetos de tipo producto

--- evaluacionBackend/Main.java
+++ evaluacionBackend/Main.java
@@ -1,35 +1,82 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main (String[] args) {
-        EmpleadoTipo1 eT1 = new EmpleadoTipo1("Mauricio Reyes", 26, 1);
-        EmpleadoTipo2 eT2 = new EmpleadoTipo2("Juan Victor", 30, 5);
-        EmpleadoTipo3 eT3 = new EmpleadoTipo3("Alejandro García", 32, 2);
+        EmpleadoTipo1 mauricioT1 = new EmpleadoTipo1("Mauricio Reyes", 26, 1);
+        EmpleadoTipo1 danielT1 = new EmpleadoTipo1("Daniel Manuel", 20, 1);
+        EmpleadoTipo1 marianaT1 = new EmpleadoTipo1("Mariana Sánchez", 18, 3);
+
+        EmpleadoTipo2 juanT2 = new EmpleadoTipo2("Juan Victor", 30, 5);
+        EmpleadoTipo2 fernandaT2 = new EmpleadoTipo2("Fernanda Gazcón", 32, 2);
+        EmpleadoTipo2 marisolT2 = new EmpleadoTipo2("Marisol Ruíz", 27, 1);
+
+        EmpleadoTipo3 isellaT3 = new EmpleadoTipo3("Isella Guzmán", 25, 1);
+        EmpleadoTipo3 isaiasT3 = new EmpleadoTipo3("Isaias Almidon", 26, 2);
+        EmpleadoTipo3 alejandroT3 = new EmpleadoTipo3("Alejandro García", 32, 2);
 
         Producto p1 = new Producto("Galletas Saladas");
+        Producto p2 = new Producto("Agua de Sabor");
+        Producto p3 = new Producto("Cereal Crujiente");
+        Producto p4 = new Producto("Aceite");
+        Producto p5 = new Producto("Servitoalla");
+
+        List<Producto> listaProductos = new ArrayList<>();
+        listaProductos.add(p1);
+        listaProductos.add(p2);
+        listaProductos.add(p3);
+        listaProductos.add(p4);
+        listaProductos.add(p5);
+
+        ArrayList listaEmpleados = new ArrayList<>();
+        listaEmpleados.add(mauricioT1);
+        listaEmpleados.add(danielT1);
+        listaEmpleados.add(marianaT1);
+        listaEmpleados.add(juanT2);
+        listaEmpleados.add(fernandaT2);
+        listaEmpleados.add(marisolT2);
+        listaEmpleados.add(isellaT3);
+        listaEmpleados.add(isaiasT3);
+        listaEmpleados.add(alejandroT3);
+
+        Utileria util = new Utileria();
         
         System.out.println("");
 
-        System.out.println("Nombre: " + eT1.getNombreCompleto());
-        System.out.println("Tipo: " + eT1.getTipo());
-        System.out.println("Pago: " + eT1.recibirPago(200.1));
+        System.out.println("Nombre: " + mauricioT1.getNombreCompleto());
+        System.out.println("Tipo: " + mauricioT1.getTipo());
+        System.out.println("Pago: " + mauricioT1.recibirPago(200.1));
 
         System.out.println("");
 
-        System.out.println("Nombre: " + eT2.getNombreCompleto());
-        System.out.println("Tipo: " + eT2.getTipo());
-        System.out.println("Pago: " + eT2.recibirPago(10.5));
+        System.out.println("Nombre: " + juanT2.getNombreCompleto());
+        System.out.println("Tipo: " + juanT2.getTipo());
+        System.out.println("Pago: " + juanT2.recibirPago(10.5));
         
         System.out.println();
 
-        System.out.println("Nombre: " + eT3.getNombreCompleto());
-        System.out.println("Tipo: " + eT3.getTipo());
-        System.out.println("Pago: " + eT3.recibirPago(700.0));
+        System.out.println("Nombre: " + alejandroT3.getNombreCompleto());
+        System.out.println("Tipo: " + alejandroT3.getTipo());
+        System.out.println("Pago: " + alejandroT3.recibirPago(700.0));
 
         System.out.println();
-
+        
         System.out.println(p1.toString());
-
+        
         System.out.println(p1.venderUnProducto());
         
         System.out.println(p1.venderProducto(15));
+        
+        System.out.println();
+        
+        System.out.println(util.imprimirHora());
+        
+        System.out.println();
+        
+        util.cadenaMayusculas("Hola");
+        
+        util.cadenaMinusculaInversa("Hola a todos los invitados!");
+        
+        System.out.println(util.getRegistroUso());
     }
 }
--- evaluacionBackend/Utileria.java
+++ evaluacionBackend/Utileria.java
@@ -1,5 +1,16 @@
+import java.time.LocalDateTime;
+
 public class Utileria {
-    // - Contar cuantas veces ha sido utilizada 
+    // - Contar cuantas veces ha sido utilizada
+    private Integer registroUso = 0;
+
+    public Integer getRegistroUso() {
+        return registroUso;
+    }
+
+    public void setRegistroUso(Integer registroUso) {
+        this.registroUso = registroUso;
+    }
 
     // MÉTODOS:
     // - Recibir lista de productos, indicar cuál tiene mayor precio.
@@ -8,8 +19,37 @@ public class Utileria {
     //     - Recibir lista de productos, retornar los métodos anteriores
 
     // - Imprimir la hora actual
+    public String imprimirHora() {
+        String hora;
+        registrarUso();
+        LocalDateTime actual = LocalDateTime.now();
+        if(actual.getHour() < 10) {
+            hora = "Hora actual: 0" + actual.getHour();
+        } else {
+            hora = "Hora actual: " + actual.getHour();
+        }
+        return hora;
+    }
+
+    public void registrarUso() {
+        setRegistroUso(getRegistroUso() + 1);
+    }
+
     // - Recibir lista empleados, eliminar todos los tipo 3, es necesario saber el nombre del empleado que esta eliminando.
     
     // - Recibir cadena de texto, retornar cadena de texto en mayúsculas.
+    public void cadenaMayusculas(String cadena) {
+        registrarUso();
+        System.out.println(cadena.toUpperCase());
+    }
+    
     // - Recibir cadena de texto, retornar cadena de texto en minúsculas e inversa.
+    public void cadenaMinusculaInversa(String cadena) {
+        registrarUso();
+        String cadenaInvertida = "";
+        for (int x = cadena.length() -1; x >= 0; x--) {
+            cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+        }
+        System.out.println(cadenaInvertida.toLowerCase());
+    }
 }
