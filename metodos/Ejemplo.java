b7f901856698f4a72b441e79f9d6e2c0a3a8ac7c
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-11-22 23:24:37 -0600

    31. Agrega ejemplo explicativo sobre métodos void con return;

---
+++ metodos/Ejemplo.java
@@ -0,0 +1,20 @@
+public class Ejemplo {
+    void metodoEjemplo() {
+        int i;
+        for (i = 0; i < 20; i++) {
+            if (i == 10) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public void metodoVariosReturns() {
+        int x = 0;
+        if (x == 0) {
+            // procesa
+            return;
+        } else {
+            return;
+        }
+    }
+}
